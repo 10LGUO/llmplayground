dcd12781e98df126d3a33f7b2b9dfc2251522044
gzn <gzndamon@gmail.com>
2026-03-10 01:45:50 -0700

    Fix bigram.py to resolve input.txt relative to script location

--- nanoGPT/bigram.py
+++ nanoGPT/bigram.py
@@ -1,3 +1,4 @@
+import os
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -15,7 +16,7 @@ eval_iters = 200
 torch.manual_seed(1337)
 
 # wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
-with open('input.txt', 'r', encoding='utf-8') as f:
+with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
@@ -43,9 +44,13 @@ def get_batch(split):
     x, y = x.to(device), y.to(device)
     return x, y
 
+# Telling pytorch everything in this function we will not call .backward() on
 @torch.no_grad()
+# Estimate the loss of the model on the train and validation sets by averaging the loss over the evaluation iterations
 def estimate_loss():
     out = {}
+    # Used if the model has dropout or batch normalization layers, etc.
+    # Q: What are these layers?
     model.eval()
     for split in ['train', 'val']:
         losses = torch.zeros(eval_iters)
